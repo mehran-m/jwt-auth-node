c6ec214fa24b5c1ca15de99b9201fe78bc40d64d
ozmau5 <59406425+ozmau5@users.noreply.github.com>
2020-09-13 19:17:24 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// Import Routes
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  () => {
+    console.log("connected to DB");
+  }
+);
+
+// Middleware
+app.use(express.json());
+
+// Route Middlewares
+app.use("/api/user", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
