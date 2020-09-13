602d29caab1fb2e513c744fde87e7fa276a54bce
ozmau5 <59406425+ozmau5@users.noreply.github.com>
2020-09-13 19:34:11 +1000

    correct schema types

--- model/User.js
+++ model/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, min: 6, max: 255 },
-  email: { type: String, required: true, min: 6, max: 255 },
-  password: { type: String, required: true, min: 6, max: 1024 },
+  name: { type: String, required: true, minlength: 6, maxlength: 255 },
+  email: { type: String, required: true, minlength: 6, maxlength: 255 },
+  password: { type: String, required: true, minlength: 6, maxlength: 1024 },
   date: { type: Date, default: Date.now },
 });
 
